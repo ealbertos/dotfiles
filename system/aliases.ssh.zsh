54d9c69fe1a00e239e7519e12a54206a51220de2
ealbertos <albertos.eduardo@gmail.com>
2016-11-04 18:23:48 -0600

    Update aliases

--- system/aliases.ssh.zsh
+++ system/aliases.ssh.zsh
@@ -15,23 +15,9 @@ alias src="cd src"
 alias dropbox="bash HOME=$HOME/.dropbox-alt /Applications/Dropbox.app/Contents/MacOS/Dropbox &"
 alias gw="grunt watch"
 
-
-# Boxen commands
-alias offnginx='boxen --disable-service nginx'
-alias onnginx='boxen --enable-service nginx'
-alias offpg='boxen --disable-service postgresql'
-alias onpg='boxen --enable-service postgresql'
-alias offmysql='boxen --disable-service mysql'
-alias onmysql='boxen --enable-service mysql'
 alias bunicorn='bundle exec unicorn -c config/unicorn.rb'
 alias binstall='bundle install'
 alias bupdate='bundle update'
 
 #Server alias
-alias centaurus="ssh root@centaurus.wearemonkeys.com"
-alias affenbits="ssh affenbit@affenbits.com"
 alias codn="ssh root@services.codnlabs.com"
-
-#Ctags
-alias ctags="/opt/boxen/homebrew/bin/ctags"
-alias createtags="ctags -R --exclude=.git --exclude=log *"
