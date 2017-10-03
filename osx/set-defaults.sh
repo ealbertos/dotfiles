3956b64b8ef7049c119914a1f346fab4b5414a01
ealbertos <albertos.eduardo@gmail.com>
2017-10-02 19:31:35 -0500

    Update

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -58,7 +58,7 @@ defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 
 # Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
+#defaults write com.apple.dock launchanim -bool false
 
 # Don’t show Dashboard as a Space
 defaults write com.apple.dock dashboard-in-overlay -bool true
