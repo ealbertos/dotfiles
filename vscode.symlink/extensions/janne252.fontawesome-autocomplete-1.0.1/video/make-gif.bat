5a748cff0a7500465663e34e73c079cf25d41535
Eduardo Albertos <eduardo@codn.mx>
2021-02-16 09:38:45 -0600

    Update

--- vscode.symlink/extensions/janne252.fontawesome-autocomplete-1.0.1/video/make-gif.bat
+++
@@ -1,2 +0,0 @@
-start /wait ffmpeg -y -i .\demo-autocomplete.mp4 -vf fps=30,scale=1024:-1:flags=lanczos,palettegen palette.png
-start /wait ffmpeg -y -i .\demo-autocomplete.mp4 -i palette.png -filter_complex "fps=30,scale=1024:-1:flags=lanczos[x];[x][1:v]paletteuse" demo-autocomplete.gif
